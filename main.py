b27ceb20b3523bf8e29cf089f65d8f3340f86e43
ThatNerd404 <bcotterman06@gmail.com>
2025-09-23 20:23:48 -0500

    moving around logging functionality to keep the project compartementalized

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 from recommender import Recommender
 
 def main():
-    print("your mom is probably a nice person.")
-    R = Recommender()
+    
+    # Setup logger and rotating file handler
+    logger = logging.getLogger("logger")
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(
+    os.path.join("Logs","log.log"), maxBytes=100000, backupCount=5, encoding="utf-8")
+    formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s',"%Y-%m-%d %H:%M:%S")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    R = Recommender(logger)
 
 if __name__ == "__main__":
     main()
--- recommender.py
+++ recommender.py
@@ -1,21 +1,9 @@
-import logging
-from logging.handlers import RotatingFileHandler
 import pandas as pd
 import os
 
 class Recommender():
-    def __init__(self):
-
-        # Setup logger and rotating file handler
-        self.logger = logging.getLogger("logger")
-        self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(
-        os.path.join("Logs","log.log"), maxBytes=100000, backupCount=5, encoding="utf-8")
-        formatter = logging.Formatter(
-        '%(asctime)s - %(levelname)s - %(message)s',"%Y-%m-%d %H:%M:%S")
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-
+    def __init__(self,logger):
+        self.logger = logger
         self.logger.debug("Finished Initialization")
         self.dataset = pd.read_csv(os.path.join("datasets","data.csv"))
         print(self.dataset)
